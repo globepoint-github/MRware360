e2bc41ddd995930755582f5d67fe8f4514bf7acd
cheayunxLee <lee79876@naver.com>
2026-03-04 18:23:20 +0900

    메타데이터 삭제 후 재배포

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import dynamic from 'next/dynamic';
 import '../app/globals.css';
 import '../app/common.css';
-import type { Metadata } from "next";
-export { metadata } from './metadata';
 
 const inter = Inter({ subsets: ["latin"] });
 const Header = dynamic(() => import('@/components/Header'), { ssr: false });
